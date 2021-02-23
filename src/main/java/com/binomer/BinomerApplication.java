c29c4ccbb28d962a863c622c641caa4b68fa2ddf
SkanderSERSI <sersi.skander077@gmail.com>
2021-02-23 15:16:23 +0100

    init

---
+++ src/main/java/com/binomer/BinomerApplication.java
@@ -0,0 +1,13 @@
+package com.binomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binomer/BinomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
